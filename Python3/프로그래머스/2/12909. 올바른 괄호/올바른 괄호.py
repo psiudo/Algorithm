a4bab77deb5825c0faa03a25e11754830d7a89c8
psiudo <pswudo@gmail.com>
2024-07-29 16:21:03 +0900

    [level 2] Title: 올바른 괄호, Time: 6.49 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
