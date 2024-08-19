ae7043603525c4bf180ef58df23fecf3b154a972
psiudo <pswudo@gmail.com>
2024-08-20 05:10:53 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- C++/프로그래머스/1/42862. 체육복/체육복.cpp
+++ C++/프로그래머스/1/42862. 체육복/체육복.cpp
@@ -4,34 +4,34 @@
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
-    // 여벌 체육복이 있지만 도난당한 학생을 처리합니다.
-    vector<int> new_lost;
-    vector<int> new_reserve;
+    vector<int> updatedLost;
+    vector<int> updatedReserve;
     
-    for (int l : lost) {
-        if (find(reserve.begin(), reserve.end(), l) != reserve.end()) {
-            reserve.erase(find(reserve.begin(), reserve.end(), l));
+    // 여벌 체육복이 있지만 도난당한 학생을 처리합니다.
+    for (int lostStudent : lost) {
+        if (find(reserve.begin(), reserve.end(), lostStudent) != reserve.end()) {
+            reserve.erase(find(reserve.begin(), reserve.end(), lostStudent));
         } else {
-            new_lost.push_back(l);
+            updatedLost.push_back(lostStudent);
         }
     }
 
-    new_reserve = reserve;
+    updatedReserve = reserve;
 
     // 체육복을 빌려줄 수 있는 경우를 처리합니다.
-    sort(new_reserve.begin(), new_reserve.end());  // 빌려주는 학생을 순서대로 정렬
-    for (int r : new_reserve) {
-        auto it = find(new_lost.begin(), new_lost.end(), r - 1);
-        if (it != new_lost.end()) {
-            new_lost.erase(it);  // 앞번호 학생에게 빌려줌
+    sort(updatedReserve.begin(), updatedReserve.end());  // 빌려주는 학생을 순서대로 정렬
+    for (int reserveStudent : updatedReserve) {
+        auto it = find(updatedLost.begin(), updatedLost.end(), reserveStudent - 1);
+        if (it != updatedLost.end()) {
+            updatedLost.erase(it);  // 앞번호 학생에게 빌려줌
         } else {
-            it = find(new_lost.begin(), new_lost.end(), r + 1);
-            if (it != new_lost.end()) {
-                new_lost.erase(it);  // 뒷번호 학생에게 빌려줌
+            it = find(updatedLost.begin(), updatedLost.end(), reserveStudent + 1);
+            if (it != updatedLost.end()) {
+                updatedLost.erase(it);  // 뒷번호 학생에게 빌려줌
             }
         }
     }
 
     // 체육수업을 들을 수 있는 학생 수를 반환합니다.
-    return n - new_lost.size();
+    return n - updatedLost.size();
 }
