fc156de013925e6bfc1709c6cca215f72dac806f
psiudo <pswudo@gmail.com>
2025-02-19 16:21:41 +0900

    [Silver V] Title: 숫자 카드, Time: 500 ms, Memory: 111720 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,28 +1,21 @@
+import sys
+input = sys.stdin.readline
+output = sys.stdout.write
 
 N = int(input())
-sangeun = sorted(list(map(int, input().split())))
+sangeun = {} 
+for num in map(int, input().split()):
+    sangeun[num] = 1 
 
 M = int(input())
-num_card = list(map(int, input().split()))
+num_card = map(int, input().split())
 
 
-def binary_search(arr, target, left, right):
-    while left <= right:
-        mid = (left + right) // 2
+result = []
+for num in num_card:
+    if num in sangeun:
+        result.append("1")
+    else:
+        result.append("0")
 
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1
-
-
-for i in range(M):
-    k = binary_search(sangeun, num_card[i], 0, N - 1)
-    if num_card[i] == sangeun[k] :
-        print('1', end=' ')
-    elif k == -1:
-        print('0', end=' ')
+output(" ".join(result) + "\n")
