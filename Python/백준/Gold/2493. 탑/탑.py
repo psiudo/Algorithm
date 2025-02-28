b2314396001f5751b644cedfa2dc536de0724cd3
psiudo <pswudo@gmail.com>
2025-03-01 08:56:17 +0900

    [Gold V] Title: 탑, Time: 436 ms, Memory: 122492 KB -BaekjoonHub

--- Python/백준/Gold/2493. 탑/탑.py
+++ Python/백준/Gold/2493. 탑/탑.py
@@ -3,18 +3,16 @@ input = sys.stdin.readline
 
 N = int(input())
 towers = list(map(int, input().split()))
-ans = [0] * N  # 각 탑에서 수신하는 탑의 번호 (1-indexed)
+stack = []
+result = [0] * N
 
 for i in range(N):
-    j = i - 1  # 바로 왼쪽 탑부터 검사
-    # 현재 탑보다 작거나 같은 탑은 점프로 건너뛰기
-    while j >= 0 and towers[j] <= towers[i]:
-        # ans[j]는 1-indexed이므로, 0-indexed로 만들기 위해 -1
-        j = ans[j] - 1  
-    # j가 유효하면 j+1이 답, 아니면 0
-    if j >= 0:
-        ans[i] = j + 1
-    else:
-        ans[i] = 0
+    # 현재 탑보다 낮은 탑들은 스택에서 제거
+    while stack and towers[stack[-1]] < towers[i]:
+        stack.pop()
+    # 스택이 비어있지 않다면, 스택의 최상단 탑이 현재 탑의 레이저 신호를 수신하는 탑
+    result[i] = stack[-1] + 1 if stack else 0
+    # 현재 탑 인덱스를 스택에 추가
+    stack.append(i)
 
-print(" ".join(map(str, ans)))
+print(" ".join(map(str, result)))
