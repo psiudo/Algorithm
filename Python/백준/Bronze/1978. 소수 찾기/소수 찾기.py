72be9764b6742790f4a5886b1de1d6001aa97a02
psiudo <pswudo@gmail.com>
2025-03-01 17:20:28 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,14 @@
+N = int(input())
+num_list = list(map(int, input().split()))
+cnt =0 
+for num in num_list :
+    if num == 1 : 
+        continue
+    
+    for i in range(2, num) :
+        if num%i == 0 :
+            break
+    else :
+        cnt += 1
+
+print(cnt)
