5149b2b6109643fec58614093b9a278c3ae51fe5
psiudo <pswudo@gmail.com>
2025-03-02 02:06:41 +0900

    [Bronze II] Title: 소수, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- Python/백준/Bronze/2581. 소수/소수.py
+++ Python/백준/Bronze/2581. 소수/소수.py
@@ -1,18 +1,20 @@
+import math
 M = int(input())
-if M == 1 :
-    M = 2
 N = int(input())
-num_list = list(range(M, N+1))
-
-for i in range(2 , int(N**0.5)+1) :
-    start = max(i*i, ((M+i-1)//i)*i)
-    for j in range(start, N+1, i) :
-        if j in num_list :
-            num_list.remove(j)
-
-
-if len(num_list) == 0 : 
-    print('-1')
-else :    
-    print(sum(num_list))
-    print(min(num_list))
+if M < 2:
+    M = 2
+size = N - M + 1
+is_prime = [True] * size
+for i in range(2, int(math.sqrt(N)) + 1):
+    start = max(i * i, ((M + i - 1) // i) * i)
+    for j in range(start, N + 1, i):
+        is_prime[j - M] = False
+prime_numbers = []
+for k in range(size):
+    if is_prime[k]:
+        prime_numbers.append(M + k)
+if prime_numbers:
+    print(sum(prime_numbers))
+    print(prime_numbers[0])
+else:
+    print(-1)
