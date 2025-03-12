df0b224e65a1b12580cc11a7948c3636f421e7ca
psiudo <pswudo@gmail.com>
2025-03-12 15:07:51 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 556 ms, Memory: 55464 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,34 @@
+import sys
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    sorted_arr = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_arr.append(left[i])
+            i += 1
+        else:
+            sorted_arr.append(right[j])
+            j += 1
+
+    sorted_arr.extend(left[i:])
+    sorted_arr.extend(right[j:])
+    return sorted_arr
+
+N = int(sys.stdin.readline())
+points = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+sorted_points = merge_sort(points)
+
+sys.stdout.write("\n".join(f"{x} {y}" for x, y in sorted_points) + "\n")
