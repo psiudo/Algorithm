f508b512842f4adc86c84ac2421b40104d69bffb
psiudo <pswudo@gmail.com>
2025-03-12 17:08:46 +0900

    [Silver V] Title: 나이순 정렬, Time: 160 ms, Memory: 46020 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -2,10 +2,10 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 users = []
-for i in range(N):
+for _ in range(N):
     age, name = input().split()
-    users.append((int(age), i, name))
-users.sort(key=lambda x: (x[0], x[1]))
-for age, _, name in users:
+    users.append((int(age), name))
+# No explicit index needed because sort is stable
+users.sort(key=lambda x: x[0])
+for age, name in users:
     sys.stdout.write(f"{age} {name}\n")
-    
