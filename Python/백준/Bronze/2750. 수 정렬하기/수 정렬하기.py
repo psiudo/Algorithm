0e6ead5e02a00fde1ec3941b6a254645088bbe0f
psiudo <pswudo@gmail.com>
2025-03-12 18:39:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ Python/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,10 +1,15 @@
 N = int(input())
 arr = [int(input()) for _ in range(N)]
 
-for i in range(N-1):
-    for j in range(N-1-i):
-        if arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+for i in range(1, N):
+    key = arr[i]
+    j = i - 1
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+    arr[j + 1] = key
 
 for num in arr:
     print(num)
+        
