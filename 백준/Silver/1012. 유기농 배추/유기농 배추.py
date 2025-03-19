447e2c39e8c125aa44a489454dd879f04b2b50df
psiudo <pswudo@gmail.com>
2025-03-19 17:01:03 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,35 @@
+
+import sys
+input = sys.stdin.readline
+
+def dfs(x, y):
+    stack = [(x, y)]
+
+    while stack:
+        x, y = stack.pop()
+
+        for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            if 0<= nx < N and 0 <= ny < M and visited[nx][ny] == 0 and farm[nx][ny] == 1:
+                stack.append((nx, ny))
+                visited[nx][ny] = 1
+                
+T = int(input())
+
+for ts in range(1, T + 1):
+    M, N, K = map(int, input().split())
+    farm = [[0] * M for _ in range(N)]
+    visited = [[0] * M for _ in range(N)]
+    count = 0
+
+    for _ in range(K):
+        x, y = map(int, input().split())
+        farm[y][x] = 1
+
+    for i in range(N):
+        for j in range(M):
+            if farm[i][j] == 1 and visited[i][j] == 0:
+                count += 1
+                dfs(i, j)
+
+    print(count)
