400b738c05f4cdd0b1b96f39de4192c8a7870b95
psiudo <pswudo@gmail.com>
2025-03-19 19:59:39 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 35076 KB -BaekjoonHub

--- Python/백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ Python/백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,22 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
-labyrinth = [list(map(int, list(input().strip()))) for _ in range(N)]
-# 각 행마다 하나의 정수로 방문 여부를 비트마스크로 관리 (초기값: 0은 모두 미방문)
-visited = [0] * N
+Labyrinth = [list(map(int, list(input().strip()))) for _ in range(N)]
+visited = [[0]*M for _ in range(N)]
 
-def mark_visited(i, j):
-    global visited
-    visited[i] |= (1 << j)
-
-def is_visited(i, j):
-    return (visited[i] & (1 << j)) != 0
-
-queue = deque()
-queue.append((0, 0, 1))  # (x, y, 거리)
-mark_visited(0, 0)
+count = 1
+queue = deque([(0, 0, count)])
 
 while queue:
-    x, y, dist = queue.popleft()
-    if x == N-1 and y == M-1:
-        print(dist)
+    x, y, count = queue.popleft()
+    if x == N-1 and y == M-1 :
+        print(count)
         break
-    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+    for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
         nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < M:
-            if not is_visited(nx, ny) and labyrinth[nx][ny] == 1:
-                mark_visited(nx, ny)
-                queue.append((nx, ny, dist + 1))
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and Labyrinth[nx][ny] == 1:
+            visited[nx][ny] = True
+            queue.append((nx, ny, count+1))
+    
