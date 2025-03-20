d796ad8812ff1ca516d503384a3e75e908da67f2
psiudo <pswudo@gmail.com>
2025-03-20 20:04:06 +0900

    [Silver I] Title: 나이트의 이동, Time: 3652 ms, Memory: 35948 KB -BaekjoonHub

---
+++ Python/백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -0,0 +1,28 @@
+
+import sys
+from collections import deque
+
+T = int(input())
+
+for ts in range(T) :
+    size = int(input())
+    start = list(map(int, input().split()))
+    final = list(map(int, input().split()))
+    
+    board = [[0]*size for _ in range(size)]
+    visited = [-1]*(size**2)
+    cnt = 0
+    queue = deque([[start[0], start[1], cnt]])
+    visited[size*start[1] + start[0]] = 0
+    
+    while queue :
+        x, y, cnt = queue.popleft()
+        if x == final[0] and y == final[1] :
+            print(cnt)
+            break
+        cnt += 1
+        for dx, dy in [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)] :
+            next_x, next_y = x + dx, y + dy
+            if 0 <= next_x <= size - 1 and 0 <= next_y <= size - 1 and visited[next_y*size + next_x ] < 0 : 
+                queue.append([next_x, next_y, cnt])
+                visited[next_y*size + next_x ] = cnt
