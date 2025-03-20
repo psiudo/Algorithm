010b8df3e096248257328626eff5f285c01ef846
psiudo <pswudo@gmail.com>
2025-03-20 20:09:15 +0900

    [Silver I] Title: 나이트의 이동, Time: 3592 ms, Memory: 35252 KB -BaekjoonHub

--- Python/백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ Python/백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -11,18 +11,16 @@ for ts in range(T) :
     
     board = [[0]*size for _ in range(size)]
     visited = [-1]*(size**2)
-    cnt = 0
-    queue = deque([[start[0], start[1], cnt]])
+    queue = deque([[start[0], start[1]]])
     visited[size*start[1] + start[0]] = 0
     
     while queue :
-        x, y, cnt = queue.popleft()
+        x, y = queue.popleft()
         if x == final[0] and y == final[1] :
-            print(cnt)
+            print(visited[y*size + x])
             break
-        cnt += 1
         for dx, dy in [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)] :
             next_x, next_y = x + dx, y + dy
             if 0 <= next_x <= size - 1 and 0 <= next_y <= size - 1 and visited[next_y*size + next_x ] < 0 : 
-                queue.append([next_x, next_y, cnt])
-                visited[next_y*size + next_x ] = cnt
+                queue.append([next_x, next_y])
+                visited[next_y*size + next_x ] = visited[y*size + x] + 1
