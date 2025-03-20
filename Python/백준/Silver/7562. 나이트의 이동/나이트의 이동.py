4537c29065a14a81e19e623b8039e845d0629cb6
psiudo <pswudo@gmail.com>
2025-03-20 20:23:59 +0900

    [Silver I] Title: 나이트의 이동, Time: 3512 ms, Memory: 35268 KB -BaekjoonHub

--- Python/백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ Python/백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,7 +1,7 @@
-
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 T = int(input())
 
 for ts in range(T) :
@@ -10,17 +10,20 @@ for ts in range(T) :
     final = list(map(int, input().split()))
     
     board = [[0]*size for _ in range(size)]
-    visited = [-1]*(size**2)
-    queue = deque([[start[0], start[1]]])
-    visited[size*start[1] + start[0]] = 0
+    visited = [False]*(size**2)
+    cnt = 0
+    queue = deque([(start[0], start[1], cnt)])
+    visited[size*start[1] + start[0]] = True
     
     while queue :
-        x, y = queue.popleft()
+        x, y, cnt = queue.popleft()
         if x == final[0] and y == final[1] :
-            print(visited[y*size + x])
+            print(cnt)
             break
+
+        cnt += 1
         for dx, dy in [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)] :
             next_x, next_y = x + dx, y + dy
-            if 0 <= next_x <= size - 1 and 0 <= next_y <= size - 1 and visited[next_y*size + next_x ] < 0 : 
-                queue.append([next_x, next_y])
-                visited[next_y*size + next_x ] = visited[y*size + x] + 1
+            if 0 <= next_x <= size - 1 and 0 <= next_y <= size - 1 and not visited[next_y*size + next_x ] : 
+                queue.append((next_x, next_y, cnt))
+                visited[next_y*size + next_x ] = True
