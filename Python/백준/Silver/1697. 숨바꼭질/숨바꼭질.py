834d5bcfef2d4c1091386d3b088b2fa4e47aafc9
psiudo <pswudo@gmail.com>
2025-03-20 17:26:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 37656 KB -BaekjoonHub

--- Python/백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ Python/백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,17 +1,24 @@
+import sys
 from collections import deque
-N, K = map(int, input().split())
-size = 100001
-visited = [False] * size
-queue = deque([(N, 0)])
-visited[N] = True 
+input = sys.stdin.readline  # 혹시 몰라서 유지
 
-while queue :
-    cur, elapse = queue.popleft()
-    if cur == K :
-            print(elapse)
-            break
-    for next in [cur+1, cur-1, 2*cur] :
-        if 0 <= next <= size-1 and not visited[next] :
-            queue.append((next, elapse+1))
-            visited[next] = True
-        
+N, K = map(int, input().split())
+
+def bfs():
+    queue = deque([N])  # (x, time) 튜플 안 쓰고 그냥 x만 저장
+    visited[N] = 0  # elapse 시간 저장
+
+    while queue:
+        x = queue.popleft()
+        time = visited[x]
+        if x == K:
+            return time  # 방문 시간 그대로 출력
+
+        for dx in [1, -1, 2]:
+            nx = x + dx if dx != 2 else x * 2
+            if 0 <= nx < 100001 and visited[nx] == -1:  # visited를 -1로 초기화
+                queue.append(nx)
+                visited[nx] = time + 1  # 시간 바로 저장
+
+visited = [-1] * 100001  # -1로 초기화하면 `False` 체크보다 빠름
+print(bfs())
