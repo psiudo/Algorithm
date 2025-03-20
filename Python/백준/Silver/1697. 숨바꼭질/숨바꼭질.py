d8283a42956092c0cb07884425725a3b7264d08b
psiudo <pswudo@gmail.com>
2025-03-20 17:12:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 232 ms, Memory: 39108 KB -BaekjoonHub

---
+++ Python/백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+size = max(N, K)*2 + 1
+visited = [False] * size
+queue = deque([(N, 0)])
+visited[N] = True 
+
+while queue :
+    cur, elapse = queue.popleft()
+    if cur == K :
+            print(elapse)
+            break
+    for next in [2*cur, cur+1, cur-1] :
+        if 0 <= next <= size-1 and not visited[next] :
+            queue.append((next, elapse+1))
+            visited[next] = True
+        
+
+
