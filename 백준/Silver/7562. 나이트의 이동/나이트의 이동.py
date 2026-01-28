179ae93f016317f6bf4ea2a7f2eccf847f055fe4
psiudo <pswudo@gmail.com>
2026-01-28 15:11:38 +0900

    [Silver I] Title: 나이트의 이동, Time: 2804 ms, Memory: 123848 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,52 +1,28 @@
-from collections import deque
-import sys
-input = sys.stdin.read
+T = int(input())
+for _ in range(T):
+    l = int(input())
+    sx, sy = map(int, input().split())
+    gx, gy = map(int, input().split())
 
-# 나이트의 8가지 이동 방향
-moves = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+    # 나이트의 8가지 이동 방향
+    dv = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+    q = [(sx, sy)]
+    v = [[0] * l for _ in range(l)]
+    v[sx][sy] = 1
+    dist = [[0] * l for _ in range(l)]
 
-def bfs_knight(l, start, end):
-    if start == end:
-        return 0
+    while q :
+        x, y = q.pop(0)
 
-    queue = deque([start])
-    visited = [[False] * l for _ in range(l)]
-    visited[start[0]][start[1]] = True
-    distance = [[0] * l for _ in range(l)]
-
-    while queue:
-        x, y = queue.popleft()
-
-        for dx, dy in moves:
+        for dx, dy in dv:
             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < l and 0 <= ny < l and not visited[nx][ny]:
-                visited[nx][ny] = True
-                distance[nx][ny] = distance[x][y] + 1
-                queue.append((nx, ny))
-
-                if (nx, ny) == end:
-                    return distance[nx][ny]
-
-    return -1  # 도달 불가능한 경우 (문제 조건상 발생하지 않음)
-
-def main():
-    data = input().split()
-    T = int(data[0])
-    index = 1
-    results = []
-
-    for _ in range(T):
-        l = int(data[index])
-        start = (int(data[index + 1]), int(data[index + 2]))
-        end = (int(data[index + 3]), int(data[index + 4]))
-        index += 5
-
-        result = bfs_knight(l, start, end)
-        results.append(result)
-
-    for res in results:
-        print(res)
-
-if __name__ == "__main__":
-    main()
+            # 범위 및 방문 조건 체크
+            if 0 <= nx <= l-1 and 0 <= ny <= l-1 and not v[nx][ny]:
+                v[nx][ny] = 1
+                dist[nx][ny] = dist[x][y] + 1
+                q.append((nx, ny))
+                # 여기서 종료조건가능
+                if (nx, ny) == (gx, gy):
+                    break
+    print(dist[gx][gy])
