87dbee33d4167743bfdc7b19e2fe954fb1f6407d
psiudo <pswudo@gmail.com>
2026-01-28 19:34:07 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 132 ms, Memory: 111508 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,39 +1,61 @@
-
+######################################
 N, M = map(int, input().split())
 
-board = []
-B_first = "BWBWBWBW"
-W_first = "WBWBWBWB"
-cnt_list = []
-def pattern_B(i, j, cnt, line) :
-    if line == 9 :
-        return cnt
+grid = [[]*M for _ in range(N)]
+for row in range(N) :
+    st = input()
+    for s in st :
+        if s == "B" :
+            grid[row].append(1)
+        else :
+            grid[row].append(0)
 
-    for p in range(0, 8) :
-        if board[i][j + p] == B_first[p]:
-            continue
-        else:
-            cnt += 1
-    return pattern_W(i + 1, j, cnt, line + 1)
+######################################
+########################## 검은색이 1 ############################
+check1 = [] # 흰색시작
+for i in range(M) :
+    if i % 2 == 1 :
+        check1.append(1)
+    else :
+        check1.append(0)
+check2 = [] #검은색시작
+for i in range(M) :
+    if i % 2 == 1 :
+        check2.append(0)
+    else :
+        check2.append(1)
+#######################################
+ans = []
+for x in range(N+1-8) :
+    for y in range(M+1-8) :
+        cnt1 = 0
+        cnt2 = 0
+        for i in range(8) :
+            for j in range(8) :
+                if i%2 == 0 :
+                    if grid[x+i][y+j] != check2[j] :
+                        cnt1 += 1
+                    else :
+                        cnt2 += 1
+                else :
+                    if grid[x+i][y+j] != check1[j] :
+                        cnt1 += 1
+                    else :
+                        cnt2 += 1
+        cnt3 = 0
+        cnt4 = 0
+        for i in range(8) :
+            for j in range(8) :
+                if i%2 == 0 :
+                    if grid[x+i][y+j] != check1[j] :
+                        cnt3 += 1
+                    else :
+                        cnt4 += 1
+                else :
+                    if grid[x+i][y+j] != check2[j] :
+                        cnt3 += 1
+                    else :
+                        cnt4 += 1
+        ans.append(min(cnt1, cnt2, cnt3, cnt4))
 
-def pattern_W(i, j, cnt, line) :
-    if line == 9 :
-        return cnt
-
-    for p in range(0, 8) :
-        if board[i][j + p] == W_first[p]:
-            continue
-        else:
-            cnt += 1
-    return pattern_B(i + 1, j, cnt, line + 1)
-
-
-for i in range(N):
-    board.append(input())
-
-for i in range(0, N - 7):
-    for j in range(0, M - 7):
-        cnt_list.append(pattern_B(i, j, 0, 1))
-        cnt_list.append(pattern_W(i, j, 0, 1))
-
-print(min(cnt_list))
+print(min(ans))
