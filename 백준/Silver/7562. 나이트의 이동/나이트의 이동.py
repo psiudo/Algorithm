0e35a1c8cd372d818ae88335f264ae244a047af9
psiudo <pswudo@gmail.com>
2026-01-28 15:17:56 +0900

    [Silver I] Title: 나이트의 이동, Time: 296 ms, Memory: 115352 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -7,22 +7,21 @@ for _ in range(T):
     # 나이트의 8가지 이동 방향
     dv = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
     q = [(sx, sy)]
-    v = [[0] * l for _ in range(l)]
-    v[sx][sy] = 1
     dist = [[0] * l for _ in range(l)]
 
     while q :
         x, y = q.pop(0)
 
+        # 여기에서 종료조건해야 한번 다른 데 가다가 돌아오는 경우 없다.
+        if (x, y) == (gx, gy):
+            break
+
         for dx, dy in dv:
             nx, ny = x + dx, y + dy
 
             # 범위 및 방문 조건 체크
-            if 0 <= nx <= l-1 and 0 <= ny <= l-1 and not v[nx][ny]:
-                v[nx][ny] = 1
+            if 0 <= nx <= l-1 and 0 <= ny <= l-1 and not dist[nx][ny]:
                 dist[nx][ny] = dist[x][y] + 1
                 q.append((nx, ny))
-                # 여기서 종료조건가능
-                if (nx, ny) == (gx, gy):
-                    break
+
     print(dist[gx][gy])
