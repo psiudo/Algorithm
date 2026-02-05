eb6b245221257f40d37f3d01a2ae446124544ffe
psiudo <pswudo@gmail.com>
2026-02-05 12:23:10 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 110920 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,14 +1,45 @@
 N = int(input())
-arr = [int(input()) for _ in range(N)]
+raw_lst = []
+for _ in range(N) :
+    raw_lst.append(int(input()))
+####################################################
+def mergesort(s_idx, e_idx, lst) :
+    # # 기저 리스트
+    # if e_idx - s_idx <= 1 :
+    #     return lst
+    """
+    이렇게 하면 반환할 곳이 없는 첫 호출에서
+    정렬하지 않고 반환한다.
+    항상, 첫호출 - 중간 호출 - 마지막 호출을 모두 정상 작동하는지 생각하자
+    """
+    if len(lst) == 1 :
+        return lst
 
 
-for i in range(1, N):
-    key = arr[i]
-    j = i - 1
-    while j >= 0 and arr[j] > key:
-        arr[j + 1] = arr[j]
-        j -= 1
-    arr[j + 1] = key
+    l, m, r = 0, len(lst)//2 , len(lst)
+    # l부터 m-1까지 / m부터 r까지 : 홀수개라면 마지막 r인덱스가 남는다.
+    lst1 = mergesort(l, m, lst[l:m])
+    lst2 = mergesort(m, r, lst[m:r])
 
-for num in arr:
-    print(num)
+    up_lst = [] # 위로 반환해 줄 리스트
+
+    left_lst_ptr, right_lst_ptr = 0, 0
+    while left_lst_ptr <= m-1 and right_lst_ptr <= r-m-1 :
+        if lst1[left_lst_ptr] <= lst2[right_lst_ptr] :
+            up_lst.append(lst1[left_lst_ptr])
+            left_lst_ptr += 1
+        else :
+            up_lst.append(lst2[right_lst_ptr])
+            right_lst_ptr += 1
+
+    if left_lst_ptr >= right_lst_ptr :
+        up_lst = up_lst + lst2[right_lst_ptr:]
+    elif left_lst_ptr <= right_lst_ptr :
+        # up_lst + lst1[left_lst_ptr:] 연산했으면 반환하기!!!!!!!!!
+        up_lst = up_lst + lst1[left_lst_ptr:]
+
+    return up_lst
+
+ans = mergesort(0, len(raw_lst)-1, raw_lst)
+for i in range(N) :
+    print(ans[i])
