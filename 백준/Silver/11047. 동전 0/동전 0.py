11f2ca446c559c36ac84a61e1a8aa65ebb8713f5
psiudo <pswudo@gmail.com>
2026-02-09 11:09:48 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,18 +1,21 @@
 N, K = map(int, input().split())
-lst = []
+coins = []
 for _ in range(N) :
-    lst.extend( [int(input())] )
-lst.sort(reverse=True) # 정렬까지 완료 in-place 함수
+    coins.append(int(input()))
 
-idx, cnt = 0, 0
-while idx <= N-1 :
-    if lst[idx] <= K :
-        K -= lst[idx]
-        cnt += 1
-    else :
-        idx += 1
-
-    if K == 0 :
+# K보다 작은 최초의 coin 찾기
+for idx, coin in enumerate(coins[::-1]) :
+    if K >= coin :
         break
+
+# idx는 N - 1에서 가장 먼저 break 걸린 것을 빼면 된다.
+idx = N - 1 - idx
+cnt = 0
+while K > 0 : # K == 0 이라면 종료되도록
+    if coins[idx] > K :
+        idx -= 1
+        continue
+    cnt += K // coins[idx]
+    K = K % coins[idx]
 
 print(cnt)
